1233901a314ef1c83c8a68c1b1d64d272a2ee7d9
omjadhav3714 <omjadhav963@gmail.com>
2022-10-17 13:52:22 +0530

    signup UI added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Home from './Home';
 import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import About from './About';
-import Quiz from './Quiz';
+// import Quiz from './Quiz';
 import Contact from './Contact';
 import "./App.css";
+import SignUp from './SignUp';
 
 const App = () => {
     return (
@@ -14,8 +15,9 @@ const App = () => {
          <Switch> 
             <Route exact path="/" component={Home} />  
             <Route exact path="/about" component={About} />  
-            <Route exact path="/quiz" component={Quiz} />  
+            {/* <Route exact path="/quiz" component={Quiz} />   */}
             <Route exact path="/contact" component={Contact} /> 
+            <Route exact path="/signup" component={SignUp} />
              
             < Redirect to="/" />    
         </Switch>
